e281179bdb25bd10c14f01ab15895d05430784b1
eunice kim <oonis86@gmail.com>
2019-10-25 02:16:25 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,11 +1,11 @@
 def mario
-
-  puts 
+  phrase = "It's-a me, Mario!"
+  puts phrase
 end
 
 def toadstool
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
-  puts 
+  puts status
 end
 
 def link
